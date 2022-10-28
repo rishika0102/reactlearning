84ecb0d8c82773137f6ea7d87f453daeb9ed4a11
rishika0102 <rishika0102>
2022-10-28 18:36:32 +0530

    react day 5

---
+++ react-learning-2/react-learning-2/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import HookCounter from './components/HookCounter';
+
+function App() {
+  return (
+    <div className="App">
+      Learn React
+      <HookCounter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-learning-2/react-learning-2/src/components/ClassCounter.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class ClassCounter extends Component {
+  render() {
+    return (
+      <div>
+      form component
+      </div>
+    )
+  }
+}
+export default ClassCounter
